16ada94232eacd00ce2e99ef91cb78cb947ee4e9
simonyost <simonyost@gmail.com>
2016-04-12 19:35:15 -0500

    started

---
+++ quiz.js
@@ -0,0 +1,13 @@
+// variables
+var correct = 0;
+
+// prompt
+var answer1 = prompt("ask a question...");
+
+// if statement
+if ( answer1.toUpperCase() === 'MAGIC' ) {
+ correct += 1;
+}
+
+// output results
+document.write("<p>write something to page...</p>");
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Instructions
+
+1. Study the 4 sample pieces of code functions
+2. Make a quiz program that...
+** Asks 5 questions
+** 
+
+---
+## Documentation
+
+JavaScript Documentation can be found on WC3 here:
+
+http://www.w3schools.com/js/default.asp
